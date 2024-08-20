2a946e5f8c22fcd7197e1a512fdbffb0e16b2cd3
aegnx <farajirasa@gmail.com>
2024-08-20 14:04:50 +0330

    Updated install.sh

--- install.sh
+++ install.sh
@@ -1,24 +1,7 @@
 set -e
-echo "Welcome To The Ramsay Installer!"
-sleep 1
-echo "Downloading binary..."
-sleep 1
-curl -LO https://github.com/aegnx/ramsay/releases/download/release020/ramsay
-sleep 1
-echo "copying binary to /bin"
-sleep 1
-echo "Please Input your Password if not in su!"
-sleep 0.1
-sudo cp ramsay /usr/bin/ramsay
-sleep 1
-rm ramsay
-sleep 1
-echo "Succesfully copied!"
-sleep 1
-echo "Giving it Execution Perms, again, enter your password if you arent in su"
-sleep 1
-sudo chmod +x /usr/bin/ramsay
-sleep 1
+echo "Downloading binary from github releases..."
+curl -sL https://github.com/aegnx/ramsay/releases/download/release020/ramsay -o $HOME/.local/bin/ramsay
+echo "giving it execution permissions"
+chmod +x ~/.local/bin/ramsay
 echo "Done! enjoy."
-sleep 1
 exit
