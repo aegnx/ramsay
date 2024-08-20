3db7df70c77c3d8c0e2a686138e6ef356df4a718
aegnx <farajirasa@gmail.com>
2024-08-20 13:32:39 +0330

    updated (you are gonna curl this for the install)

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
-set -e
-echo "welcome! hope you ran it as sudo"
+et -e
+echo "running sudo su for admin (this program copies the binary into your /usr/bin folder)"
+sudo su
 echo "downloading binary."
 curl -LO https://github.com/aegnx/ramsay/releases/download/release020/ramsay
 echo "copying binary to /bin, hope the binary is in the same folder you're in"
@@ -8,3 +9,4 @@ echo "succesfully copied"
 echo "giving it execution perms"
 chmod +x /usr/bin/ramsay
 echo "done! enjoy."
+exit
